d7b5460a4eb7420e0cebe715182334f648d4e8d7
ttony0321 <48801180+ttony0321@users.noreply.github.com>
2022-04-25 20:38:51 +0900

    Settings.py

--- board/cron.py
+++ board/cron.py
@@ -158,6 +158,7 @@ def Humorunicraw():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     url = "http://web.humoruniv.com/board/humor/list.html?table=pds"
     driver.get(url)
